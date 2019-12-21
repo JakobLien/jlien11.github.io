3da48dcf3516b43e8d7d542d6afad67393592d20
jlien11 <29503509+jlien11@users.noreply.github.com>
2019-12-21 11:47:40 +0100

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<html>
+	<head>
+		<title> sdio </title>
+	</head>
+	<body>
+		<php>
+			$ipaddress = '';
+			    if ($_SERVER['HTTP_CLIENT_IP'])
+				$ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+			    else if($_SERVER['HTTP_X_FORWARDED_FOR'])
+				$ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+			    else if($_SERVER['HTTP_X_FORWARDED'])
+				$ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+			    else if($_SERVER['HTTP_FORWARDED_FOR'])
+				$ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+			    else if($_SERVER['HTTP_FORWARDED'])
+				$ipaddress = $_SERVER['HTTP_FORWARDED'];
+			    else if($_SERVER['REMOTE_ADDR'])
+				$ipaddress = $_SERVER['REMOTE_ADDR'];
+			    else
+				$ipaddress = 'UNKNOWN';
+			    echo $ipaddress;
+		</php>
+	</body>
+</html>
